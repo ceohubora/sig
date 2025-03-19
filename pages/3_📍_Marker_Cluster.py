096386f125067d1aa64f16121ca976341af82733
ceohubora <ceohubora@gmail.com>
2025-03-19 13:20:44 -0300

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -3,7 +3,25 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
+with st.expander("See source code"):
+    with st.echo():
 
+        m = leafmap.Map(center=[-4.957474,-41.486756], zoom=10)
+        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+
+        m.add_geojson(regions, layer_name="US Regions")
+        m.add_points_from_xy(
+            cities,
+            x="longitude",
+            y="latitude",
+            color_column="region",
+            icon_names=["gear", "map", "leaf", "globe"],
+            spin=True,
+            add_legend=True,
+        )
+
+m.to_streamlit(height=700)
 
 st.title("SIG - PIRACURUCA")
 
