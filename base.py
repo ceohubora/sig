d0c8130527e615aef434e2e7c3c5b1c1ac5d4ed7
ceohubora <ceohubora@gmail.com>
2025-03-26 17:07:36 -0300

    Update base.py

--- base.py
+++ base.py
@@ -121,4 +121,4 @@ with st.expander("Ver código fonte"):
             '11': 'Rondônia', '12': 'Acre', '13': 'Amazonas', '14': 'Roraima', '15': 'Pará',
             '16': 'Amapá', '17': 'Tocantins', '21': 'Maranhão', '22': 'Piauí', '23': 'Ceará',
             '24': 'Rio Grande do Norte', '25': 'Paraíba', '26': 'Pernambuco', '27': 'Alagoas', '28': 'Sergipe',
-            '29': 'Bahia', '31': 'Minas Gerais', '32': 'Espírito Santo', '33': 'Rio
+            '29': 'Bahia', '31': 'Minas Gerais', '32': 'Espírito Santo', '33': 'Rio de Janeiro',
